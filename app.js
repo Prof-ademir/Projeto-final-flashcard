3e417460f7afc94b296b821bba0b353220574106
Prof-ademir <ademirdefreitas2020@gmail.com>
2024-11-16 20:21:04 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+function criacartao(categoria, pergunta, respostas) {
+   let container = document.getElementById('container')
+   let cartao = document.createElement('article')
+   cartao.className = 'cartao'
+
+cartao.innerHTML = `
+<div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+       <div class="cartao__conteudo__pergunta">
+      <p>${pergunta}</p>
+         </div>
+      <div class="cartao__pergunta__respostas">
+ <p>${respostas}</p> 
+    </div>
+   </div>   
+   ` 
+   let respostaEstaVisivel = false
+
+   function viraCartao(){
+      respostaEstaVisivel = !respostaEstaVisivel 
+      cartao.classList.toggle('active', respostaEstaVisivel)
+
+   }
+
+   cartao.addEventListener('click', viraCartao)
+
+
+  container.appendChild(cartao)    
+   
+}
---
+++ assets/img/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programação',
+    'O que é javascript?',
+    'Javascript é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos de executas algumas tarefas'
+)
+
+criacartao(
+    'lingua inglesa',
+    'como se diz oi em inglês?',
+    'Oi em inglês é HI'
+)
